125b4382d6c79f18daa990978bb905d11e2002a4
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-26 16:50:19 -0400

    Add form-edit folder

---
+++ form-edit/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Form from './Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
